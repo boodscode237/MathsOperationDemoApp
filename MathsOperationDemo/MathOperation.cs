ac9ce92aceddb74c5549cfb3aff88f8f71d57a3a
BriceDon <65740750+boodscode237@users.noreply.github.com>
2021-05-22 01:26:44 +0500

    Add project files.

---
+++ MathsOperationDemo/MathOperation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsOperationDemo
+{
+    public class MathOperation
+    {
+        public double Add(double a, double b)
+        {
+            double output = a + b;
+            return output;
+        }
+        public double Subtract(double a, double b)
+        {
+            double output = a - b;
+            return output;
+        }
+        public double Divide(double a, double b)
+        {
+            double output = 0;
+            if (b != 0)
+            {
+                output = a / b;
+            }
+            return output;
+        }
+        public double Multiply(double a, double b)
+        {
+            double output = a * b;
+            return output;
+        }
+    }
+}
---
+++ MathsOperations.Tests/MathsOperationsTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathsOperationDemo;
+using Xunit;
+
+namespace MathsOperations.Tests
+{
+    public class MathsOperationsTests
+    {
+        [Theory]
+        [InlineData(4.0,2.0,6.0)]
+        public void AddShouldReturnExpectedValue(double num1, double num2, double expected)
+        {
+            // Arrange
+            MathOperation calculate = new MathOperation();
+
+            // Act
+            double actual = calculate.Add(num1, num2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(4.0, 2.0, 2.0)]
+        public void SubtractShouldReturnExpectedValue(double num1, double num2, double expected)
+        {
+            // Arrange
+            MathOperation calculate = new MathOperation();
+
+            // Act
+            double actual = calculate.Subtract(num1, num2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(4.0, 2.0, 8.0)]
+        public void MultiplyShouldReturnExpectedValue(double num1, double num2, double expected)
+        {
+            // Arrange
+            MathOperation calculate = new MathOperation();
+
+            // Act
+            double actual = calculate.Multiply(num1, num2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(12.0, 2.0, 6.0)]
+        [InlineData(12, 0, 0)]
+        public void DivideShouldReturnExpectedValue(double num1, double num2, double expected)
+        {
+            // Arrange
+            MathOperation calculate = new MathOperation();
+
+            // Act
+            double actual = calculate.Divide(num1, num2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
